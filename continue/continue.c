4c2b584a69fdeebec20cfa58c8831ff104b22814
Amir <ahosseinnemat@gmail.com>
2020-05-13 15:17:14 +0430

    add continue sample

---
+++ continue/continue.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    for(int i = 1 ; i <= 10 ; i++)
+    {
+        if ((i%2) == 0)
+        {
+            continue;
+        }
+        printf("Hello i\'m odd!. %d\n",i);
+    }
+    return 0;
+}
