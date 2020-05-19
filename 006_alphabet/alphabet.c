4f7f1c8eb26f9f78cf31cffa49c33f5513044c54
Amir <ahosseinnemat@gmail.com>
2020-05-19 17:18:30 +0430

    Add new sample

---
+++ 006_alphabet/alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "ctype.h"
+
+int main() {
+  
+    printf("Enter a character : \n");
+    char ch = getchar();
+
+    if (isalpha(ch)) {
+        printf("the %c is a alphabet\n", ch);
+    } else {
+        printf("your input is not alphabet\n");
+    }
+}
